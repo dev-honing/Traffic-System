9eb000afd4d29328195d84971ec81bd3f017bd47
Tech1997 <dev.honing@gmail.com>
2023-10-26 14:20:47 +0900

    요청 처리에 대한 콘솔 메시지 작성하고 index.html 생성

--- my-express-project/app.js
+++ my-express-project/app.js
@@ -6,15 +6,16 @@ app.use(express.static('public'));
 
 // GET 요청 핸들러
 app.get('/', (req, res) => {
-  res.send('GET 요청 처리'); // 클라이언트에 응답을 보냅니다.
+  console.log('GET 요청을 받았습니다.'); // GET 요청을 받았음을 콘솔에 출력
+  res.send('GET 요청 처리');
 });
 
 // POST 요청 핸들러
 app.post('/', (req, res) => {
-  res.send('POST 요청 처리'); // 클라이언트에 응답을 보냅니다.
+  console.log('POST 요청을 받았습니다.'); // POST 요청을 받았음을 콘솔에 출력
+  res.send('POST 요청 처리');
 });
 
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
-
