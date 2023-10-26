88c1a8e446b01ae35abe3eee25420058aa64d1b4
Tech1997 <dev.honing@gmail.com>
2023-10-26 14:39:44 +0900

    GET과 POST를 처리하는 버튼을 만들고 잘 동작하는지 확인하기

--- my-express-project/app.js
+++ my-express-project/app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(express.static('public'));
 
 // GET 요청 핸들러
 app.get('/', (req, res) => {
-  console.log('GET 요청을 받았습니다.'); // GET 요청을 받았음을 콘솔에 출력
+  console.log('GET 요청을 받았습니다.');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/get-request', (req, res) => {
+  console.log('GET 요청을 받았습니다.');
   res.send('GET 요청 처리');
 });
 
+
 // POST 요청 핸들러
-app.post('/', (req, res) => {
-  console.log('POST 요청을 받았습니다.'); // POST 요청을 받았음을 콘솔에 출력
+app.post('/post-request', (req, res) => {
+  console.log('POST 요청을 받았습니다.');
   res.send('POST 요청 처리');
 });
 
